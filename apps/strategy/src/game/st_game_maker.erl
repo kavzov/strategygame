34521f2521b2e99a364596f51cd1300d53736452
Igor Kavzov <igor.kavzov@gmail.com>
2019-05-29 15:58:13 +0300

    Change get_game functions

--- apps/strategy/src/game/st_game_maker.erl
+++ apps/strategy/src/game/st_game_maker.erl
@@ -3,7 +3,7 @@
 
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3,
-         connect/2, add_new_game/2, get_games/1, add_game/2, get_game/1, get_game_id/1, play_game/2, get_all_games/0, del_game/1, del_player_from_waitlist/1
+         connect/2, add_new_game/2, get_games/1, add_game/2, get_game/1, get_game_by_srv/1, get_game_id/1, play_game/2, get_all_games/0, del_game/1, del_player_from_waitlist/1
 ]).
 
 % -record(state, {}).
@@ -21,11 +21,14 @@ add_player_to_waitlist(Socket) ->
 del_player_from_waitlist(Socket) ->
     gen_server:call(?MODULE, {del_player_from_waitlist, Socket}).
 
-get_game(GameSrv) ->
-    gen_server:call(?MODULE, {get_game, GameSrv}).
+get_game(GameId) ->
+    gen_server:call(?MODULE, {get_game, GameId}).
+
+get_game_by_srv(GameSrv) ->
+    gen_server:call(?MODULE, {get_game_by_srv, GameSrv}).
 
 get_game_id(GameSrv) ->
-    gen_server:call(?MODULE, {get_game, GameSrv}).
+    gen_server:call(?MODULE, {get_game_id, GameSrv}).
 
 get_all_games() ->
     gen_server:call(?MODULE, get_all_games).
@@ -94,7 +97,10 @@ handle_call({play_game, GameId, PlayerId, PlayerName, Wallet, Battles, Won, Rati
             {reply, game_not_found, State}
     end;
 
-handle_call({get_game, GameSrv}, _From, State) ->
+handle_call({get_game, GameId}, _From, State) ->
+    {reply, maps:get(GameId, State), State};
+
+handle_call({get_game_by_srv, GameSrv}, _From, State) ->
     Res = maps:filter(fun(_GameId, Game) ->
         maps:get(srv, Game) =:= GameSrv end, State),
     {reply, Res, State};
@@ -117,29 +123,6 @@ handle_call({get_games, Status}, _From, State) ->
     ),
     {reply, Reply, State};
 
-% handle_call({add_game, GameSrv, Players}, _From, #state{games = Games} = State) ->
-%     % erlang:monitor(process, PlayerSrv),
-%     {reply, ok, State#state{games = maps:put(GameSrv, Players, Games)}};
-
-% handle_call({add_player_to_waitlist, Socket}, _From, #state{waitlist = WaitList} = State) ->
-%     {reply, ok, State#state{waitlist = lists:append(WaitList, [Socket])}};
-
-% handle_call({del_player_from_waitlist, Socket}, _From, #state{waitlist = WaitList} = State) ->
-%     {reply, ok, State#state{waitlist = lists:delete(Socket, WaitList)}};
-
-% handle_call({get_game, GameSrv}, _From, #state{games = Games} = State) ->
-%     {reply, maps:find(GameSrv, Games), State};
-
-% handle_call(get_all_games, _From, #state{games = Games} = State) ->
-%     {reply, maps:to_list(Games), State};
-
-% handle_call({get_opponent, _Raiting}, _From, #state{waitlist = WaitList} = State) ->
-%     % Match making logic -- maps:filter -> map with matching Player
-%     case WaitList of
-%         [] -> {reply, not_found, State};
-%         [OpntSock] -> {reply, {ok, OpntSock}, State}
-%     end;
-
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
@@ -149,9 +132,6 @@ handle_cast({del_game, GameSrv}, State) ->
         maps:get(srv, G) =:= GameSrv end, State),
     [GameId] = maps:keys(Game),
     {noreply, maps:remove(GameId, State)};
-
-% handle_cast({del_game, GameSrv}, #state{games = Games} = State) ->
-%     {noreply, State#state{games = maps:remove(GameSrv, Games)}};
 
 handle_cast(_Request, State) ->
     {noreply, State}.
