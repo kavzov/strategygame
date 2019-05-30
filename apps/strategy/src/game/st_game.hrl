ab088d7529b8e2feb1c30ceedd7b8b88de308ef3
Igor Kavzov <igor.kavzov@gmail.com>
2019-05-30 14:18:36 +0300

    Add table command

--- apps/strategy/src/game/st_game.hrl
+++ apps/strategy/src/game/st_game.hrl
@@ -3,18 +3,22 @@ server_msg() ->
 	{ok, MaxInt} = application:get_env(strategy, max_battle_field_length),
     [Min, Max] = lists:map(fun(Val) -> integer_to_binary(Val) end, [MinInt, MaxInt]),
 	<<"
-+=====================================================+
-| Command  | Action                                   |
-+=====================================================+
-| list     | update list waiting players              |
-+-----------------------------------------------------+
-| play ID  | play game number ID (if there is)        |
-+-----------------------------------------------------+
-| game W H | start new game with dimension W x H      |
-|          | W - width, H - height (min: ", Min/binary, ", max: ", Max/binary, ")  |
-+-----------------------------------------------------+
-| bet      | make a bet                               |
-+-----------------------------------------------------+
-| exit     | leave the server                         |
-+-----------------------------------------------------+
++======================================================+
+| Command   | Action                                   |
++======================================================+
+| list      | update list of players and games         |
++------------------------------------------------------+
+| info      | current player info (rating, money, ...) |
++------------------------------------------------------+
+| table     | players table with achievements          |
++------------------------------------------------------+
+| play ID   | play game number ID (if there is)        |
++------------------------------------------------------+
+| game W H  | start new game with dimension W x H      |
+|           | W - width, H - height (min: ", Min/binary, ", max: ", Max/binary, ")  |
++------------------------------------------------------+
+| bet G P $ | make a bet: GameID, PlayerID, $um        |
++------------------------------------------------------+
+| exit      | leave the server                         |
++------------------------------------------------------+
 \r\n">>.
